94b5fe21a570a2048ac0620efbee113d868a0ff8
sandorex (thorium) <rzhw3h@gmail.com>
2023-07-24 11:34:58 +0200

    Test

--- scripts/download-fonts.ts
+++
@@ -1,9 +0,0 @@
-// download latest build
-const url = 'https://github.com/sandorex/compacity-fonts/releases/download/latest/compacity-block.zip'
-
-const https = require('https')
-const fs = require('fs');
-
-for(let i=0; i<10; i++)
-  https.get(url, resp => resp.pipe(fs.createWriteStream(`./fonts${i}.jpeg`)));
-
